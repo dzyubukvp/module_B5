d907675b370fbb4da127eb192c8694d719f36f3b
dzyubukvp <dzyubukvp@yandex.ru>
2022-11-28 16:44:12 +0400

    add cofim new projekt Krestiki-Noliki

--- krestikinoliki.py
+++ krestikinoliki.py
@@ -13,7 +13,7 @@ ura_victory = [[0,1,2],
                [0,4,8],
                [2,4,6]
                ]
-
+# рисуем поле битвы
 def pole_vidim():
     print(pole[0], end=" ")# 1 линия
     print(pole[1], end=" ")
@@ -27,6 +27,51 @@ def pole_vidim():
     print(pole[7], end=" ")
     print(pole[8])
 
+#функция обработки события выбора ячейки поля
 def napole(step,symbol):
     ind = pole.index(step)
     pole[ind] = symbol
+
+
+def get_result():
+    win = ""
+
+    for i in ura_victory:
+        if pole[i[0]] == "X" and pole[i[1]] == "X" and pole[i[2]] == "X":
+            win = "X"
+        if pole[i[0]] == "O" and pole[i[1]] == "O" and pole[i[2]] == "O":
+            win = "O"
+
+
+    return win
+
+
+# Основная программа
+game_over = False
+player1 = True
+
+while game_over == False:
+
+    # 1. Показываем карту
+    pole_vidim()
+
+    # 2. Спросим у играющего куда делать ход
+    if player1 == True:
+        symbol = "X"
+        step = int(input("Ход игрока № 1: "))
+    else:
+        symbol = "O"
+        step = int(input("Ход игрока № 2: "))
+
+    napole(step, symbol)  # делаем ход в указанную ячейку
+    win = get_result()  # определим победителя
+    if win != "":
+        game_over = True
+    else:
+        game_over = False
+
+    player1 = not (player1)
+
+# Игра окончена. Покажем карту. Объявим победителя.
+pole_vidim()
+print("Победил игрок играющий ", win)
